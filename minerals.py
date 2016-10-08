322e38915843f1ffa5157b89a76116d5ddcd9ef2
capybara43 <jake.perrett@gmail.com>
2016-10-08 14:55:08 +0100

    1st released version of the mineral checker program
    Python 2.7 was used. The code description gives a pretty good overview.

---
+++ minerals.py
@@ -0,0 +1,55 @@
+minerals = [{'name': 'quartz', 'lower hardness': 7,'upper hardness':7,'transparency':'true','Streak':'white','magnetism':'false','fun fact':'there are many impurities of quartz including amethyst which the ancient greeks believed would stop them from getting drunk'},
+            {'name': 'gypsum', 'lower hardness': 2,'upper hardness':2,'transparency':'true','Streak':'white','magnetism':'false','fun fact':' a robot sent into space has found evidence of gypsum dunes towards the north of mars'},
+            {'name': 'calcite', 'lower hardness': 3,'upper hardness':3,'transparency':'true','Streak':'white','magnetism':'false','fun fact':'calcite was used in World War Two for gun sites and people have made experiments to use calcite as a cloak of invisibility'},
+            {'name': 'magnetite', 'lower hardness': 5.5,'upper hardness':6.5,'transparency':'false','Streak':'black','magnetism':'true','fun fact':'magnetite is the most magnetic minerel in the world and it was with this mineral that humans discovered magnetism'},
+            {'name': 'variscite', 'lower hardness': 4.5,'upper hardness':4.5,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'varisite is a fairly rare mineral and is sometimes used as a semi-precious stone'},    
+            {'name': 'wavelite', 'lower hardness': 3.5,'upper hardness':4 ,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'wavelite was discovered by a pysician in 1805'},    
+            {'name': 'diamond', 'lower hardness': 10,'upper hardness':10,'transparency':'false','Streak':'colourless','magnetism':'false','fun fact':'diamond is the hardest mineral discovered and defines number ten on the Mohs scale'},
+            {'name': 'turquoise', 'lower hardness': 5,'upper hardness':7,'transparency':'false','Streak':'bluishwhite','magnetism':'false','fun fact':'the colour of turquoise varies between samples ranging from white to blue'},
+            {'name': 'magnesite', 'lower hardness': 3.5,'upper hardness':4.5,'transparency':'true','Streak':'white','magnetism':'false','fun fact':'like gypsum and calcite, magnesite is a salt. It is formed when magnesium reacts with HCO3 (carbonic acid); the same acid that calcium reacts with to make calcite (CaCO3)'},
+            {'name': 'feldspar', 'lower hardness': 6,'upper hardness':6.5,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'clay minerals can be formed by the weathering of feldspar'},
+            {'name': 'mica', 'lower hardness': 2.5,'upper hardness':3,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'mica is one of the three minerals found in granite; the other two are quartz and feldspar'},
+            {'name': 'polished diamond', 'lower hardness': 10,'upper hardness':10,'transparency':'true','Streak':'colourless','magnetism':'false','fun fact':'as diamond is the hardest mineral on the planet, people have to use special tools made out of diamond to shape it before polishing'},
+            {'name': 'impure calcite', 'lower hardness': 3,'upper hardness':3,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'calcite is often found as a different colour than white because of impurities.'},
+            {'name': 'died magnesite', 'lower hardness': 3.5,'upper hardness':4.5,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'magnesite is often died blue to look like turquise'},
+            {'name': 'impure gypsum', 'lower hardness': 2,'upper hardness':2,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'gypsum is very similar to calcite: it is translucent and it is a salt formed after calcium reacts with an acid(although different acids for different minerals'},
+            {'name': 'copper', 'lower hardness': 2.5,'upper hardness':3,'transparency':'false','Streak':'black','magnetism':'false','fun fact':' copper veins often contain small amounts of other minerals like silver'},
+            {'name': 'gold', 'lower hardness': 2.5,'upper hardness':3,'transparency':'false','Streak':'yellow','magnetism':'false','fun fact':'although gold is an element, it is extremely unlikely that you will find pure gold as it often has impurities'},
+            {'name': 'graphite', 'lower hardness': 1,'upper hardness':2,'transparency':'false','Streak':'black','magnetism':'false','fun fact':'graphite is made of carbon ,the same element diamond is made of, but it has a low hardness due to the way that the carbon atoms are arranged'}, 
+            {'name': 'Mercury', 'lower hardness': 0,'upper hardness':1,'transparency':'false','Streak':'scarlet','magnetism':'false','fun fact':'mercury is the only metal to be liquid at room temperture'},
+            {'name': 'sulphur', 'lower hardness': 1.5,'upper hardness':2.5,'transparency':'true','Streak':'yellow','magnetism':'false','fun fact':'sulphur is usally depoiseted from volcanic gas that has come from active volcanoes'},    
+            {'name': 'bornite', 'lower hardness': 3,'upper hardness':3,'transparency':'false','Streak':'brown','magnetism':'false','fun fact':'bornite can be used as a copper ore'}, 
+            {'name': 'Arsenopyrite', 'lower hardness': 5.5,'upper hardness':6,'transparency':'false','Streak':'white','magnetism':'false','fun fact':'arsenic ,a native element, often occurs in veins of Arsenopyrite'},     
+            {'name': 'chalcocite', 'lower hardness': 2.5,'upper hardness':3,'transparency':'false','Streak':'black','magnetism':'false','fun fact':'chalcocite is a common ore of copper as it is simply made of sulphur and copper'},
+            {'name': 'chalcopyrite', 'lower hardness': 2.5,'upper hardness':3,'transparency':'false','Streak':'yellow','magnetism':'false','fun fact':'chalcocite is a common ore of copper as it is simply made of sulphur and copper'},
+            {'name': 'cinnabar', 'lower hardness': 2,'upper hardness':2.5,'transparency':'false','Streak':'brown','magnetism':'false','fun fact':'cinnabar has been used as a pigment to make paint'},
+            {'name': 'cobaltite', 'lower hardness': 5.5,'upper hardness':5.5,'transparency':'false','Streak':'black','magnetism':'false','fun fact':'cobaltite is an ore of cobalt and cobalt is the core of vitamin B12'},
+            {'name': 'covellite', 'lower hardness': 1.5,'upper hardness':2,'transparency':'false','Streak':'blue','magnetism':'false','fun fact':'covellite is formed by alteration of other copper sulphides like chalcopyrite'},
+            {'name': 'erythrite', 'lower hardness': 1.5,'upper hardness':2.5,'transparency':'true','Streak':'red','magnetism':'false','fun fact':'erythrite is known as cobalt bloom because it can be useful to locate cobalt rich minerals'},
+            {'name': 'galena', 'lower hardness': 2.5,'upper hardness':2.5,'transparency':'false','Streak':'grey','magnetism':'false','fun fact':'galena has been one of the most important ores of lead since Roman times'},
+            {'name': 'jamesonite', 'lower hardness': 2,'upper hardness':3,'transparency':'false','Streak':'grey','magnetism':'false','fun fact':'jamesonite is one of the few sulphides to form needle like crystalls'},
+            {'name': 'lollingite', 'lower hardness': 5,'upper hardness':5.5,'transparency':'false','Streak':'grey','magnetism':'false','fun fact':'lollingite was named in 1845 because it was first found in the lolling district'},
+            {'name': 'marcasite', 'lower hardness': 6,'upper hardness':6.5,'transparency':'false','Streak':'grey','magnetism':'false','fun fact':'marcasite was named in 1845 after the Arabic word for pyrite as it looks very similar to pyrite'},
+            ]
+print('minerals in database')
+
+for mineral in minerals :
+    print(mineral['name'])
+
+hardness = float(input('what is the hardness? '))
+transparency = str(input('Is it transparent? '))
+streak = input('what is the streak, choose from white, blue, colourless, black, blueishwhite,brown,scarlet,yellow,red,grey or green')
+magnetism = input('Is it magnetic? ')
+
+for mineral in minerals :
+    print('checking', mineral['name'])
+    if mineral['upper hardness'] >= hardness and mineral['lower hardness'] <= hardness :
+        print('hardness match',mineral['name'])
+        if mineral['transparency'] == transparency :
+            print('transparency match',mineral['name'])
+            if mineral['Streak'] == streak :
+                print('streak match',mineral['name'])
+                if mineral['magnetism'] == magnetism :
+                    print('magnetism match',mineral['name'])                             
+                    print('your mystery mineral is ',mineral['name'])                    
+                    print (mineral ['fun fact'])                
